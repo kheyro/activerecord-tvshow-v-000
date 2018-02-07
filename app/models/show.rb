4e7116e3f597c9eaffd001b0e6c2f4e665de2186
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-07 22:24:36 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where(rating: Show.select('MAX(rating)'))
+    Show.where(rating: Show.select('MAX(rating)')).flatten
   end
 
   def self.lowest_rating
